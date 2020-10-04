7108e3d8e3a97762cd492d4ff2bb8d1b702a336c
djmatlas <djmdeveloper060796@gmail.com>
2020-10-04 15:11:37 +0530

    Add vegeta engage method

--- vegeta_core/vegeta_util.go
+++ vegeta_core/vegeta_util.go
@@ -3,8 +3,13 @@ package vegeta_core
 import (
 	"fmt"
 	vegeta "github.com/tsenart/vegeta/v12/lib"
+	models "github.com/djmgit/DeathStar/models"
 )
 
-var struct Vegeta_util {
+type struct Vegeta_util {
+	vegeta_params models.Vegeta_attack_params
+}
 
+func (vegeta_util *Vegeta_util) Engage_vegeta() (error, vegeta.Metrics) {
+	return nil, ""
 }

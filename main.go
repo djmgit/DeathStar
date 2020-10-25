69d00e5190f7e99bf8f9f92b196355533bd6c7c7
djmatlas <djmdeveloper060796@gmail.com>
2020-10-25 18:37:42 +0530

    Add flow in main

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+	"os"
 	vegetaUtil "github.com/djmgit/DeathStar/vegeta_core"
 	vegetaModels "github.com/djmgit/DeathStar/models"
 	vegeta "github.com/tsenart/vegeta/v12/lib"
@@ -25,11 +26,40 @@ func main() {
 	var isLocal bool
 	var zipFilePath string
 	var confPath string
+	var deploy string
 
 	flag.BoolVar(&isLocal, "local", false, "Denotes and DeathStar will use local zip. Zip path must be profiled")
 	flag.StringVar(&zipFilePath, "zip-file-path", "./func.zip", "Path to local Zip file containing handler")
 	flag.StringVar(&confPath, "conf", "deathstar.conf", "Path to conf file")
+	flag.BoolVar(&deploy, "deploy", false, "Deploy function to lambda and initiate attack")
 
 	flag.Parse()
+
+	if deploy == true {
+
+		if isLocal == false {
+
+			// check zip-file-path is present or not
+			if zipFilePath == "" {
+				fmt.Println("zipfile path not providing. Exiting DeathStar...")
+				os.Exit(2)
+			}
+		} else {
+			// donwload zipfile and set zip file path in zipFilePath
+		}
+
+		// Upload function and deploy
+
+		// initiate attack and display result
+
+		// clean up function
+
+		// exit DeathStar
+
+		os.Exit(0)
+	}
+
+	// Fucntion is running in lambda, initialte handler
+	lambda.Start(HandleLambdaEvent)
 
 }

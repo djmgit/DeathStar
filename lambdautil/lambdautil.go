d25df823ef6ed1ba53554796ef999f827b0f4610
djmatlas <djmdeveloper060796@gmail.com>
2020-11-13 20:47:11 +0530

    Add lambda function invokation from function

--- lambdautil/lambdautil.go
+++ lambdautil/lambdautil.go
@@ -5,9 +5,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
     "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/lambda"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
 	vegetaModels "github.com/djmgit/DeathStar/models"
-    "fmt"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type LambdaUtil struct {
@@ -87,6 +89,25 @@ func (lambdaUtil *LambdaUtil) CreateFunction() error {
 	return nil
 }
 
-func (lambdaUtil *LambdaUtil) runFunction(yamlConfig vegetaModels.YAMLConfig) error {
-	return nil
+func (lambdaUtil *LambdaUtil) runFunction(lambdaRequest vegetaModels.LambdaRequest) (error, *vegeta.Metrics) {
+
+	// create the lambda function using the provided informations
+	if lambdaUtil.AWSSession == nil {
+		err := lambdaUtil.GetAWSSession()
+		if err != nil {
+			return err, &vegeta.Metrics{}
+		}
+	}
+
+	svc := lambda.New(lambdaUtil.AWSSession)
+
+	payload, err := json.Marshal(lambdaRequest)
+	if err != nil {
+		fmt.Println("Error marshalling request...")
+		fmt.Println(err.Error())
+		return err, &vegeta.Metrics{}
+	}
+
+	_, _ == svc.Invoke(&lambda.InvokeInput{FunctionName: aws.String(lambdaUtil.LambdaFuncName), Payload: payload})
+	return nil, nil
 }

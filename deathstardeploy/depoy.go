ee3d0ee521a08b7d4e44ec40bc8568204c69ba37
djmatlas <djmdeveloper060796@gmail.com>
2020-12-05 18:07:38 +0530

    Code refactor

--- deathstardeploy/depoy.go
+++ deathstardeploy/depoy.go
@@ -3,6 +3,8 @@ package deathstardeploy
 import (
 	"gopkg.in/yaml.v2"
 	vegetaModels "github.com/djmgit/DeathStar/models"
+	"github.com/djmgit/DeathStar/lambdautil"
+	vegetaUtil "github.com/djmgit/DeathStar/vegeta_core"
 	"io/ioutil"
 	"fmt"
 	"errors"
@@ -55,6 +57,49 @@ func (deathStarDeploy *DeathStarDeploy) Start() error {
 	} else {
 		// donwload zipfile and set zip file path in zipFilePath
 	}
+
+	fmt.Println("Creating the lambda attack function...")
+	lambdaUtil := lambdautil.LambdaUtil {
+		AWSRegion: "us-east-1",
+		LambdaRole: "arn:aws:iam::253708721073:role/service-role/func-test-1-role-nyalwdp2",
+		LambdaFuncName: "func-test-2",
+		LambdaFunctionHandler: "main",
+		LambdaFunctionRuntime: "go1.x",
+		ZipFilePath: deathStarDeploy.ZipFilePath,
+	}
+	err = lambdaUtil.CreateFunction()
+	if err != nil {
+		fmt.Println("Function creation failed...")
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Function creation succeeded...")
+
+	// initiate attack and display result
+	vegAttackUtil := vegetaUtil.VegetaAttackUtils{
+		LmUtil: &lambdaUtil,
+	}
+
+	fmt.Println("Running attack...")
+	err, resultMetrics := vegAttackUtil.VegetaSeqAttack(deathStarDeploy.yamlConfig.Attacks)
+
+	fmt.Println("Attack complete...")
+	for _, result := range resultMetrics {
+		fmt.Println(result.Latencies.Mean)
+		fmt.Println("\n")
+		fmt.Println(result.Duration)
+		fmt.Println("\n")
+		fmt.Println(result.Success)
+	}
+
+	fmt.Println("Cleaning up function...")
+	err = lambdaUtil.DeleteFunction()
+	if err != nil {
+		fmt.Println("Faced error while deleting function")
+	}
+
+	fmt.Println("Exiting DeathStar...")
 
 	return nil
 }

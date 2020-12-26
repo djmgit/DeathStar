5a4b5d422bed9b3a21e05601d690f12534558685
djmatlas <djmdeveloper060796@gmail.com>
2020-12-26 12:59:45 +0530

    Fix typo

--- models/lambdaresponse.go
+++ models/lambdaresponse.go
@@ -3,6 +3,6 @@ package models
 import vegeta "github.com/tsenart/vegeta/v12/lib"
 
 type LambdaResponse struct {
-	ResultMetrics *vegeta.Metrics `json:"lambdaRole" yaml:"AttackResponseMetrics"`
-	AttackDetails LambdaRequest `json:"attacks" yaml:"attackDetails"`
+	ResultMetrics *vegeta.Metrics `json:"AttackResponseMetrics" yaml:"AttackResponseMetrics"`
+	AttackDetails LambdaRequest `json:"attackDetails" yaml:"attackDetails"`
 }

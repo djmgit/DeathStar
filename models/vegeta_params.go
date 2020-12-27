ddb96abb5807349be09e7f9987226fe0c621264b
djmatlas <djmdeveloper060796@gmail.com>
2020-12-27 20:40:40 +0530

    Add inline doc

--- models/vegeta_params.go
+++ models/vegeta_params.go
@@ -4,7 +4,10 @@ import (
 	"time"
 )
 
-// struct to hold arguments to vegeta
+// VegetaAttackParams holds fields for vegeta attack config.
+// These data are required by vegeta to carry out the attack
+// The meaning and purpose of the fields can be found in the vegeta documentaion
+
 type VegetaAttackParams struct {
 	Method string	`json:"httpMethod" yaml:"httpMethod"`
 	Url string	`json:"url" yaml:"url"`

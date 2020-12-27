f020f1830e6bbe01cd23173bf2bba9244c3d240d
djmatlas <djmdeveloper060796@gmail.com>
2020-12-27 20:31:14 +0530

    Add inline doc

--- models/yamlconfig.go
+++ models/yamlconfig.go
@@ -1,6 +1,16 @@
+
+// models contains struct definitions for various structures used through out
+// the DeathStar codebase
 package models
 
+// YAMLConfig holds the config attributes for the yaml config file
+// required to configure DeathStar in order to carry out attacks.
 type  YAMLConfig struct {
+
+	// Attacks is the list of attack configs
 	Attacks []LambdaRequest `json:"attacks" yaml:"attacks"`
+
+	// LambdaConfig is the configs for the lambda function itself that
+	// will be created in order to carry out the attack.
 	LambdaConfig LambdaFunctionConfig `json:"lambdaConfig" yaml:"lambdaConfig"`
 }

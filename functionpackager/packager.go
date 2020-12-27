083b9464c17a36a04981d79dfb4b0ebb6ebcd8c5
djmatlas <djmdeveloper060796@gmail.com>
2020-12-27 21:25:39 +0530

    Update packager

--- functionpackager/packager.go
+++ functionpackager/packager.go
@@ -1,15 +1,27 @@
 package functionpackager
 
 import (
+	"archive/zip"
+	"github.com/rs/zerolog"
 	"os"
 	"path/filepath"
 )
 
-func Packager() {
+func Packager(deathLogger zerolog.Logger) {
 	executablePath, _ := os.Executable()
 	executableName := filepath.Base(executablePath)
 	dirPath := filepath.Dir(executablePath)
 
 	zipFileName := "lambda_func.zip"
+	zipFilePath := filepath.Join(dirPath, zipFileName)
+
+	zipFile, err := os.Create(zipFileName)
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
 }
 

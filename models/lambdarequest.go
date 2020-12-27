9a0bd5b955482c340be92a49585b8b7e01f2c7e9
djmatlas <djmdeveloper060796@gmail.com>
2020-12-27 20:45:59 +0530

    Add inline doc

--- models/lambdarequest.go
+++ models/lambdarequest.go
@@ -1,7 +1,14 @@
 package models
 
+// LambdaRequest holds properties to represent a request to the deployed lambda function
 type LambdaRequest struct {
+
+	// AttackName if the name of the attack. This can be used as a unique identifier for the attack.
 	AttackName string `json:"attackName" yaml:"attackName"`
+
+	// AttackDesc is the description of the attack
 	AttackDesc string `json:"attackDesc" yaml:"attackDesc"`
+
+	// VegetaParams contains the data required by vegeta.
 	VegetaParams VegetaAttackParams `json:"vegetaAttackParams" yaml:"vegetaAttackParams"`
 }

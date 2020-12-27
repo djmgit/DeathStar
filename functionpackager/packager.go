87f73ca7b79fbae1bddf23d5045f9761c0a13b4e
djmatlas <djmdeveloper060796@gmail.com>
2020-12-27 21:42:22 +0530

    Update packager

--- functionpackager/packager.go
+++ functionpackager/packager.go
@@ -3,19 +3,19 @@ package functionpackager
 import (
 	"archive/zip"
 	"github.com/rs/zerolog"
+	"io"
 	"os"
 	"path/filepath"
 )
 
 func Packager(deathLogger zerolog.Logger) {
 	executablePath, _ := os.Executable()
-	executableName := filepath.Base(executablePath)
 	dirPath := filepath.Dir(executablePath)
 
 	zipFileName := "lambda_func.zip"
 	zipFilePath := filepath.Join(dirPath, zipFileName)
 
-	zipFile, err := os.Create(zipFileName)
+	zipFile, err := os.Create(zipFilePath)
 	if err != nil {
 		deathLogger.Fatal().Msg(err.Error())
 	}
@@ -23,5 +23,39 @@ func Packager(deathLogger zerolog.Logger) {
 
 	zipWriter := zip.NewWriter(zipFile)
 	defer zipWriter.Close()
+
+	AddToZipFile(zipWriter, executablePath, deathLogger)
+}
+
+func AddToZipFile(zipWriter *zip.Writer, filePath string, deathLogger zerolog.Logger) {
+	fileName := filepath.Base(filePath)
+
+	fileToZip, err := os.Open(filePath)
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
+
+	header.Name = fileName
+
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
+	_, err = io.Copy(writer, fileToZip)
+	if err != nil {
+		deathLogger.Fatal().Msg(err.Error())
+	}
 }
 
